93d7d2c5ba1748f1c8c459e6269ebdd6e763f7b2
lowercaselife <dansonkalaghe@gmail.com>
2021-06-17 08:32:17 -0700

    low level programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ *
+ * Return: always 0 (success)
+ */
+
+
+int main(void)
+{
+char mystr[] = "programming is like building a multilingual puzzle\n";
+puts(mystr);
+
+return (0);
+
+}
