02ce71c1642cf4dda677e22d990c1eccad8fc9c4
lowercaselife <dansonkalaghe@gmail.com>
2021-06-17 10:08:07 -0700

    low level programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int a_integer;
 long a_long;
 long long a_longlong;
 float a_float;
-printf("Size of char: %lu byte(s)\n", sizeof(a_character));
+printf("Size of a char: %lu byte(s)\n", sizeof(a_character));
 printf("Size of an int: %lu byte(s)\n", sizeof(a_integer));
 printf("Size of a long int: %lu byte(s)\n", sizeof(a_long));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(a_longlong));
