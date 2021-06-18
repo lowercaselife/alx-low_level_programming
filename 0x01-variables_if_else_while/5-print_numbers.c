aeb5d8b3e38ef41e35ccd1209e2f2eb7e86a10dc
lowercaselife <dansonkalaghe@gmail.com>
2021-06-18 11:43:36 -0700

    low level programming

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: always 0(success)
+*/
+int main(void)
+{
+int digit;
+
+for (digit = 0; digit  <= 20; c++)
+printf("%i", digit);
+putchar('\n');
+
+
+return (0);
+}
