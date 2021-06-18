8803198d64312c3c8929d9ed28bdc141e0f24093
lowercaselife <dansonkalaghe@gmail.com>
2021-06-18 10:19:36 -0700

    low level programming

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: always 0(success)
+*/
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+
+
+return (0);
+}
