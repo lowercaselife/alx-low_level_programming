764f3042da96bf6ff8c05b32ec586fc1ac829b96
lowercaselife <dansonkalaghe@gmail.com>
2021-06-22 08:40:06 -0700

    low level programming

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_alpabet_x10 - prints alphabet 10 times
+ *
+ * Return : void
+ */
+
+void print_alphabet_x10(void);
+{
+int 1, a;
+for (i = 0; i < 9; i++)
+{
+for (a = 97; a < 122; a++)
+{
+_putchar(a);
+}
+_putchar('\n')
+}
+}
