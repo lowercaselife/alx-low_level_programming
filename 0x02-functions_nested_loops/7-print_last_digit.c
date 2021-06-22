5ffff588432033dffd9624850df3d05addbc646a
lowercaselife <dansonkalaghe@gmail.com>
2021-06-22 13:01:44 -0700

    low level programming

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_last_digit - prints the last digit of a number.
+ *
+ * @n: inputs number as an integer.
+ *
+ * Return: last digit
+ */
+
+int print_last_digit(int n)
+{
+int l;
+l = n % 10;
+if (1 < 0)
+{
+_putchar(-1 + 48);
+return (-1);
+}
+else
+{
+_putchar(1 + 48);
+return (1);
+}
+}
