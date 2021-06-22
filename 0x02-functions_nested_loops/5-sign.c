3e251862479b00697fb7581027a9d214d8f17ad9
lowercaselife <dansonkalaghe@gmail.com>
2021-06-22 11:53:41 -0700

    low level programming

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "holberton"
+/**
+ * print_sign - determines if the input number is greater, equal or less than 0
+ *
+ * @n: the input number
+ *
+ * Return: (1) is greater than 0, (0) is zero, (-1) is less than 0
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+}
+_putchar(43);
+return (1);
+}
+else if (n == 0)
+{
+_putchar(48);
+return (0);
+}
+else
+{
+_putchar(45);
+return (-1);
+}
+_putchar('\n')
+}
