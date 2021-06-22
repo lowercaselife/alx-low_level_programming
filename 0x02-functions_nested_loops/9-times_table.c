b6343ba8ffecbbf78ca9bcf73a9dd4434d3fa5ff
lowercaselife <dansonkalaghe@gmail.com>
2021-06-22 13:59:39 -0700

    low level programming

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,9 +16,8 @@ for (b = 1; b <= 9; b++)
 c = a * b;
 _putchar(44);
 _putchar(32);
-{
 if (c <= 9)
-{
+}
 _putchar(32);
 _putchar(c + 48);
 }
@@ -29,6 +28,5 @@ _putchar((c % 10) + 48);
 }
 }
 _putchar('\n');
-}
 }
 }
