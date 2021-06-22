fc1a4ed12a19c2b9dbc6a19b7eb13b62ac105c69
lowercaselife <dansonkalaghe@gmail.com>
2021-06-22 10:55:39 -0700

    low level programming

---
+++ 0x02-functions_nested_loops/4-alpha.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - returns 1 if the input is an alphabet. 0 for other cases.
+ *
+ * @c: ASCII character
+ *
+ * Return: 1 if c is a letter, upercase or lowercase, 0 if not.
+ */
+
+int _isalpha(int c)
+{
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+}
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+}
