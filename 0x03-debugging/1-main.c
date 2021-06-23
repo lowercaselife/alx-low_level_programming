d319c1093329db6d252bf648207009169d93bba8
lowercaselife <dansonkalaghe@gmail.com>
2021-06-23 11:42:10 -0700

    low level programming

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 
 /**
- * main -causes an infinite loop
+ * main - causes an infinite loop
  * Return: 0
  */
 
 int main(void)
 {
 int i;
-printf("infinite loop incoming :(\n")
-i = 0
+printf("infinite loop incoming :(\n");
+i = 0;
 /*while (i < 10)*/
 /*{*/
 /*putchar(i);*/
