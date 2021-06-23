e8ffae295285744597344d88825fb246fa4f7f57
lowercaselife <dansonkalaghe@gmail.com>
2021-06-23 14:52:20 -0700

    low level programming

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,13 +2,13 @@
 #include "holberton.h"
 
 /**
- * print_remaining_days - takes a date and prints how may days are
- * left in the year, taking leap years into account 
- * @month: month in number format
- * @day: day of month
- * @year: year
- * Return: void
- */
+* print_remaining_days - takes a date and prints how may days are
+* left in the year, taking leap years into account
+* @month: month in number format
+* @day: day of the month
+* @year: year
+* Return: void
+*/
 
 void print_remaining_days(int month, int day, int year)
 {
@@ -31,7 +31,7 @@ void print_remaining_days(int month, int day, int year)
 	else
 		{
 		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n",365 - day);
+		printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
