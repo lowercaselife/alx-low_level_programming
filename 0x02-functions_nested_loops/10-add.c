1c589d72c4442892c57df393f585c90bc88cc015
lowercaselife <dansonkalaghe@gmail.com>
2021-06-23 05:09:46 -0700

    low level programming

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * add- a function that adds two integers and returns the result
+ *
+ * @a: first integer
+ *
+ * @b: second integer
+ *
+ * Return: result of addition
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
