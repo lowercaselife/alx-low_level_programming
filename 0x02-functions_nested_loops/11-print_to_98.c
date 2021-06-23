68dc2d6a30bd9b3537eb619666398c53ee1be8a2
lowercaselife <dansonkalaghe@gmail.com>
2021-06-23 06:03:13 -0700

    low level programming

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_to_98 - a function that prints all natural numbers from n to 98
+ *
+ * @n: input number
+ *
+ * Return: no return
+ */
+
+void print_to_98(int n)
+{
+if (n > 98)
+{
+for (; n > 98; n--)
+{
+printf("%d, ", n);
+}
+}
+else if (n < 98)
+{
+for (; n < 98; ++)
+{
+printf("%d, ", n);
+}
+}
+printf("%d\n", n);
+}
