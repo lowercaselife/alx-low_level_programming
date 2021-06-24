7613687291055c777399e7dd08c7ccf21b1a3ffe
lowercaselife <dansonkalaghe@gmail.com>
2021-06-24 06:07:37 -0700

    low level programming

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _is digit - checks for a digit (0 through 9)
+ * _isdigit - checks for a digit (0 through 9)
  * @c: character to be checked
  * Return: 0 or 1 according to previous check
  */
