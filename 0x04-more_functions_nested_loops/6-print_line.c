e59abb02db39e169aa07c8070e46d4db71aa8f6c
lowercaselife <dansonkalaghe@gmail.com>
2021-06-24 09:53:21 -0700

    low level programming

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * print_line - draws straight line in the terminal.
+ * @n: integer amount of '_' to be printed
+ */
+void print_line(int n)
+{
+	char ch;
+
+	for (ch = 0; ch < n; ch++)
+		_putchar('_');
+	_putchar('\n');
+}
