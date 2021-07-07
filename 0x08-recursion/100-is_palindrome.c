e75394f0d6f401810d1408775e7c829dda1987e6
lowercaselife <dansonkalaghe@gmail.com>
2021-07-07 05:40:06 -0700

    C recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -39,5 +39,5 @@ int is_palindrome(char *s)
 	len = _strlen_recursion(s);
 	if (len == 0 || *s != s[len - 1])
 		return (0);
-	return (is_palind_recursive(s, 0, len - 1));i
+	return (is_palind_recursive(s, 0, len - 1));
 }
