0c0562a06e2c1084ca2e0c721595e0c190c555dd
lowercaselife <dansonkalaghe@gmail.com>
2021-07-19 12:38:50 -0700

    struct, typedef

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,16 +1,18 @@
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
+
 /**
- * free_dog - clean the memory space frees dogs
- * @d: The pointer with the adress of the struct dogs.
- *
- * Return: void function, just free the memory allocated by dog..
+ * free_dog - free memory
+ *@d: array
+ * Description: Free memory for struct
  */
+
 void free_dog(dog_t *d)
 {
-	if (d == '\0')
-		return;
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
